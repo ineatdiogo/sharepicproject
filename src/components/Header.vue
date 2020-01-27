5d47fdf09748a775a7fa4d6539510f7a5a995232
Diogo DIALLO <ddiallo@ineat.fr>
2020-01-27 14:13:29 +0100

    Split the project by components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -49,6 +49,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 .top-nav {
     display: flex;
     flex-wrap: wrap;
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="content">
+        <Discovery />
+        <Navigation />
+        <DiscoveryBody />
+    </div>
+</template>
+
+<script>
+
+import Discovery from './Discovery.vue'
+import Navigation from './TabNavigation.vue'
+import DiscoveryBody from './DiscoveryBody.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        Discovery,
+        Navigation,
+        DiscoveryBody
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
